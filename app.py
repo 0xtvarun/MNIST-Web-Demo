cbee4a4227fb25184624970225572632879e89ef
0xtvarun <vtewari26@gmail.com>
2018-05-25 13:27:27 +0530

    update host in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 from network import Network
@@ -21,4 +22,5 @@ def api():
     return jsonify({'payload': str(out.argmax())})
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
